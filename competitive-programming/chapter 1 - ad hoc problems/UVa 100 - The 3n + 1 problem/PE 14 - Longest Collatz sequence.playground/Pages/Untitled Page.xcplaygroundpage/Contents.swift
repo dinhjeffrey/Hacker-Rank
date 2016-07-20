f4d764152d19ad2993b878ffb16cafc2c4324cf7
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-19 17:09:05 -0700

    updated with comments

--- competitive-programming/chapter 1 - ad hoc problems/UVa 100 - The 3n + 1 problem/PE 14 - Longest Collatz sequence.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ competitive-programming/chapter 1 - ad hoc problems/UVa 100 - The 3n + 1 problem/PE 14 - Longest Collatz sequence.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -30,12 +30,11 @@ import Foundation
 //    return longestChain
 //}
 
-
+// returns the amount of steps that makes it a collatz sequence (maxNum -> 1). modifies an inout cache array that holds index corresponding to their collatz length
 func collatzLength(number: Int, inout cache: [Int]) -> Int {
     var steps = 0
     var n = number
-    
-    while n >= number {
+    while n >= number { // why n >= number? anything below we already solved and added to cache array
         
         if n % 2 == 0 {
             n /= 2
@@ -44,17 +43,19 @@ func collatzLength(number: Int, inout cache: [Int]) -> Int {
             steps += 1
         }
         steps += 1
+        
     }
-    steps += cache[n]
-    cache[number] = steps
+    steps += cache[n] // when exiting the loop because n < number, we add the value of cache[index]. cache[index] contains the number of steps at each index 0,1... maxNumber-1
+    cache[number] = steps // cache[index] = steps, so cache[2] = 2. cache[3] = 6.
     
     return steps
 }
 
+// solves which number has the longest collatz sequence given a maxNumber. returns the format in tuple format of the number and amount of steps for it's collatz sequence. create an inout cache array that gets modified in the collatzLength
 func longestCollatzSequence(maxNumber: Int) -> (number: Int, steps: Int) {
     var collatzSequence = (number:0, steps:0)
     var cache: [Int] = [Int](count: maxNumber, repeatedValue: 0)
-    cache[1] = 1
+    cache[1] = 1 // [0, 1, ... maxNumber]. start from 2..< maxNumber
     
     for number in 2 ..< maxNumber {
         let steps = collatzLength(number, cache: &cache)
@@ -65,11 +66,13 @@ func longestCollatzSequence(maxNumber: Int) -> (number: Int, steps: Int) {
     return collatzSequence
 }
 
+// executes the function and prints out values in tuple format
 func euler14() {
-    let (number, _) = longestCollatzSequence(13)
-    print(number)
+    let (number, steps) = longestCollatzSequence(13)
+    print("number is \(number) and steps is \(steps)")
 }
 
+// keeps track of time
 func printTimeElapsedWhenRunningCode(operation:() -> ()) {
     let startTime = CFAbsoluteTimeGetCurrent()
     operation()
