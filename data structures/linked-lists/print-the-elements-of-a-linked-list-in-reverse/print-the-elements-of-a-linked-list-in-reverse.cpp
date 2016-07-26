41b667ea4ffdc1871c8ce22e38b6f6de1d14c00e
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-26 16:43:27 -0700

    editted print elements of a linked list in reverse

--- data structures/linked-lists/print-the-elements-of-a-linked-list-in-reverse/print-the-elements-of-a-linked-list-in-reverse.cpp
+++ data structures/linked-lists/print-the-elements-of-a-linked-list-in-reverse/print-the-elements-of-a-linked-list-in-reverse.cpp
@@ -10,32 +10,11 @@
  */
 void ReversePrint(Node *head)
 {
-    // This is a "method-only" submission.
-    // You only need to complete this method.
-    // creates nodes named current, prev, next
-    struct Node *current, *prev, *next;
-    // sets current and previous to their respective values. current is head because it is starting position while previous is where we want the head to be pointing, which is null, since we are reverse a linked list
-    current = head;
-    prev = NULL;
-    // allocate memory to an array and initialize i=0
-    int array[100], i=0;
-    while (current != NULL) {
-        // store data into an array to print out later
-        array[i] = current->data;
-        i+=1;
-        // updates next [current, link] -> [int, link] = next
-        next = current->next;
-        // updates link of current.next to value of previous
-        current->next = prev;
-        // updates previous value
-        prev = current;
-        // updates current value
-        current = next;
+    if (head == NULL) {
+        return;
     }
-    // at the end of list, sets head to the last value in linked list which is previous
-    head = prev;
-    // goes through array in reverse order and prints
-    for(int j=i-1; j>=0; j--) {
-        cout<<array[j]<<endl;
-    }
+    // recursion tree. loops through until end of linked list
+    ReversePrint(head->next);
+    // once the recursive is done, then prints head->data going back down the recursion tree.
+    printf("%d\n", head->data);
 }
