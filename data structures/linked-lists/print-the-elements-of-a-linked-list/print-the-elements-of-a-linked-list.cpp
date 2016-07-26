8bb4d0fc8adfcb5d4ff93a468c112e1dd0c3434e
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-26 09:17:20 -0700

    added print the elements of a linked-list in c++

---
+++ data structures/linked-lists/print-the-elements-of-a-linked-list/print-the-elements-of-a-linked-list.cpp
@@ -0,0 +1,17 @@
+/*
+ Print elements of a linked list on console
+ head pointer input could be NULL as well for empty list
+ Node is defined as
+ struct Node
+ {
+ int data;
+ struct Node *next;
+ }
+ */
+void Print(Node *head)
+{
+    while(head != NULL) {
+        cout<< head->data <<endl; // sets head = data  cout: Standard output stream, endl: Insert newline and flush
+        head = head->next; // sets head = next
+    }
+}
