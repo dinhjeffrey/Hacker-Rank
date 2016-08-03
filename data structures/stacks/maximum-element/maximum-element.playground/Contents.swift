3ee16c06954cc1455c3547d3f0c7f732288bb8f8
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-08-03 08:59:51 -0700

    refactored max element

--- data structures/stacks/maximum-element/maximum-element.playground/Contents.swift
+++ data structures/stacks/maximum-element/maximum-element.playground/Contents.swift
@@ -1,56 +1,72 @@
+/*              (✿ ♥‿♥)                 ╘[◉﹃◉]╕                      ᕙ(⇀‸↼‶)ᕗ
+ Ｏ(≧▽≦)Ｏ	              ❤ Amoria ❤                       ★~(◡﹏◕✿)
+ (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧            (づ￣ ³￣)づ                    (づ｡◕‿‿◕｡)づ
+ */
 import Foundation
 
-/// Returns an integer read from one line of standard input.
+// Maximum Element
+
+// returns an integer from a line of standard input
 func readInteger() -> Int {
-    guard let line = readLine() else {
-        fatalError("Unexpected end of input")
-    }
-    guard let i = Int(line) else {
-        fatalError("Invalid integer in input")
-    }
+    guard let line = readLine() else { fatalError("unable to read line") }
+    guard let i = Int(line) else { fatalError("unable to read integer") }
     return i
 }
 
-// Returns an array of integers read from one line of standard input.
+// returns an array of integers from a line of standard input
 func readIntegers() -> [Int] {
-    guard let line = readLine() else {
-        fatalError("Unexpected end of input")
-    }
-    return line.componentsSeparatedByString(" ").map{ Int($0)! }
+    guard let line = readLine() else { fatalError("unable to read line") }
+    let integers = line.componentsSeparatedByString(" ").map{ Int($0)! }
+    return integers
 }
 
+// creates a stack
 var stack = [Int]()
 
+// possible types
+enum Type: Int {
+    case one = 1, two, three
+}
+
+// performs operations on stack depending on input
+func maxElement(input: [Int]) {
+    guard let type = Type(rawValue: input[0]) else { fatalError("unknown type found") }
+    switch type {
+    case .one:
+        let element = input[1]
+        stack.append(element)
+    case .two:
+        stack.removeLast()
+    case .three:
+        guard let maxElement = stack.maxElement() else { fatalError("stack is empty") }
+        print(maxElement)
+    }
+}
+
+// reads inputs and calls maxElement
 func inputs() {
-    let queries = readInteger()
-    for _ in 0..<queries {
+    let numQueries = readInteger()
+    for _ in 0..<numQueries {
         let input = readIntegers()
         maxElement(input)
     }
 }
 
-func maxElement(input: [Int]) {
-    let type = input[0]
-    switch type {
-    case 1:
-        let element = input[1]
-        stack.append(element)
-    case 2:
-        stack.removeLast()
-    case 3:
-        if let maxElement = stack.maxElement() {
-            print(maxElement)
-        }
-    default:
-        fatalError("unknown input type")
-    }
-}
-
 inputs()
 
-/*
- 
- //Then, on the command line use something like...
- cat input00.txt | swift maximum-element.playground/Contents.swift
- 
- */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
