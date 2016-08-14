12a7f880d37cc00e27b9168fa3ac42cf5ecb1ea2
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-08-13 18:36:17 -0700

    half way poisonous plants 😱

--- data structures/stacks/poisonous-plants/Poisonous-Plants.playground/Contents.swift
+++ data structures/stacks/poisonous-plants/Poisonous-Plants.playground/Contents.swift
@@ -1,1 +1,43 @@
 import Foundation
+
+// returns an integer from one standard line of input
+func readInteger() -> Int {
+    guard let line = readLine() else { fatalError("cannot read line") }
+    guard let int = Int(line) else { fatalError("cannot convert line to int") }
+    return int
+}
+
+// returns an array of integers from one standard line of input
+func readIntegers() -> [Int] {
+    guard let line = readLine() else { fatalError("cannot read line") }
+    let ints = line.componentsSeparatedByString(" ").map{ Int($0)! }
+    return ints
+}
+
+func dayStopDying(plantPesticide: [Int]) {
+    // holds first value of plantPesticide
+    let firstValue = plantPesticide.first
+    // start from 1...lastValue
+    var plantPesticideAdjusted = plantPesticide[1..<plantPesticide.count]
+    var lastDay = false
+    while lastDay == false {
+        // keeps track of last value being compared to
+        var lastValue = firstValue
+        let alivePlants = plantPesticideAdjusted.filter{
+//            if $0 > lastValue {
+//                lastValue = $0
+//            }
+//            return $0 > lastValue
+        }
+    }
+}
+
+
+
+// reads input
+func input() {
+    let _ = readInteger()
+    let plantPesticide = readIntegers()
+    dayStopDying(plantPesticide)
+}
+
