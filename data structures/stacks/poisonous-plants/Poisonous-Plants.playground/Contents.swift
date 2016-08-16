1731609c42d9a5fdedabd57bd0247bac91411768
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-08-15 18:29:04 -0700

    working code

--- data structures/stacks/poisonous-plants/Poisonous-Plants.playground/Contents.swift
+++ data structures/stacks/poisonous-plants/Poisonous-Plants.playground/Contents.swift
@@ -15,30 +15,41 @@ func readIntegers() -> [Int] {
 }
 
 func dayStopDying(plantPesticide: [Int]) {
-    // holds first value of plantPesticide
-    let firstValue = plantPesticide.first
-    // start from 1...lastValue
-    var plantPesticideAdjusted = Array(plantPesticide[1..<plantPesticide.count])
-    var lastDay = [Int]()
-    var dayCount = 0
-    while lastDay != plantPesticideAdjusted {
-        // keeps track of last value being compared to
-        lastDay = plantPesticideAdjusted
-        var lastValue = firstValue
-        plantPesticideAdjusted = plantPesticideAdjusted.filter{
-            let temp = lastValue
-            lastValue = $0
-            return $0 < temp
+    var s = [Int](count: plantPesticide.count, repeatedValue: 0)
+    s[0] = 0
+    var k = [Int](count: plantPesticide.count, repeatedValue: 0)
+    k[0] = -1
+    
+    // stack contains last seen plant index
+    var stack = [Int]()
+    
+    // the first index has already been seen
+    stack.append(0)
+    
+    // index of current plant
+    var i = 1
+    let n = plantPesticide.count
+    while i<n {
+        // the generation plantPesticide[i] survived to
+        var generation = 0
+        // while last-seen plants are not killers of the current plant plantPesiticide[i]
+        while !stack.isEmpty && plantPesticide[stack.last!] >= plantPesticide[i] {
+            generation = max(generation, s[stack.last!])
+            stack.popLast()
         }
-        dayCount += 1
-        print(dayCount)
+        if !stack.isEmpty && s[stack.last!] > 0 && generation >= s[stack.last!] {
+            // if the potential killer of this plant died at an earlier generation
+            // we clear them off the stack
+            while !stack.isEmpty && s[stack.last!] > 0 && generation >= s[stack.last!] {
+                stack.popLast()
+            }
+        }
+        s[i] = stack.isEmpty ? 0 : generation + 1
+        k[i] = stack.isEmpty ? -1 : stack.last!
+        stack.append(i)
+        i += 1
     }
-    let dayAdjusted = dayCount - 1  // because it runs an extra time to check
-    if dayAdjusted == 1 {
-        print(0) // if days never incremented
-    } else {
-        print(dayAdjusted)
-    }
+    print(s.maxElement()!)
 }
 
 // reads input
