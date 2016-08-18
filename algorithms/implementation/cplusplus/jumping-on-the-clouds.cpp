1ac38c283fb2dd569b1b8f3d422c5f1ddbbcb1d9
Holly Ho <hollyho27@gmail.com>
2016-08-17 23:37:16 -0700

    adding jumping-on-the-clouds

---
+++ algorithms/implementation/cplusplus/jumping-on-the-clouds.cpp
@@ -0,0 +1,48 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> c(n);
+    for(int c_i = 0;c_i < n;c_i++){
+       cin >> c[c_i];
+    }
+    
+    int count=0;
+    
+    for(int i=0; i<c.size()-1; i++)
+    {
+        if(c[i+2]==0)
+        {   i+=1;
+            count++;
+        }
+        else
+           count++;
+    }
+    cout << count;
+    return 0;
+}
