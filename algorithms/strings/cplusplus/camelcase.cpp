85c3dbbf3791c9acf5c3f3a2a897686c28285855
Holly Ho <hollyho27@gmail.com>
2016-08-22 21:42:57 -0700

    adding camelcase.cpp

---
+++ algorithms/strings/cplusplus/camelcase.cpp
@@ -0,0 +1,36 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    string s;
+    cin >> s;
+    int numWords = 1;
+    for (char e: s)
+        if (e < 96)
+            numWords++;
+        
+    cout << numWords;
+}
